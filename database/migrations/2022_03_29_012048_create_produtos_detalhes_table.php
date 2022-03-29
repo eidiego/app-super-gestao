e1e43e5b08478af7577b73c0fe6044d7acb3c241
Diego Rodrigues <diegokod@gmail.com>
2022-03-28 22:50:48 -0300

    make migrations

---
+++ database/migrations/2022_03_29_012048_create_produtos_detalhes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutosDetalhesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos_detalhes', function (Blueprint $table) {
+            $table->id();
+
+            $table->float('cumprimento', 8, 2);
+            $table->float('largura', 8, 2);
+            $table->float('altura', 8, 2);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos_detalhes');
+    }
+}
