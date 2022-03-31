7471329987a5555939788418c3b245ba320f15ce
Diego Rodrigues <diegokod@gmail.com>
2022-03-30 22:53:38 -0300

    add SoftDeletes

--- app/Fornecedor.php
+++ app/Fornecedor.php
@@ -3,8 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Fornecedor extends Model
 {
+
+    use SoftDeletes;
+
     protected $table = 'fornecedores';
+    protected $fillable = ['nome', 'site', 'uf', 'email'];
+
 }
--- database/migrations/2022_03_31_014916_alter_fornecedores_softdelete.php
+++ database/migrations/2022_03_31_014916_alter_fornecedores_softdelete.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFornecedoresTable extends Migration
+class AlterFornecedoresSoftdelete extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreateFornecedoresTable extends Migration
      */
     public function up()
     {
-        Schema::create('fornecedores', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-            //$table->SoftDeletes();
+
+        Schema::table('fornecedores', function (Blueprint $table) {
+            $table->SoftDeletes();
         });
     }
 
@@ -27,6 +26,8 @@ class CreateFornecedoresTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('fornecedores');
+        Schema::table('fornecedores', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+         });
     }
 }
