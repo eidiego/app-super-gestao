66843e928be3e10dcd2fd19241969b8dcc50eeec
Diego Rodrigues <diegokod@gmail.com>
2022-03-30 23:05:05 -0300

    Add FornecedorSeeder

---
+++ database/seeds/FornecedorSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Fornecedor;
+
+class FornecedorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $fornecedor = new Fornecedor();
+        $fornecedor->nome = 'Fornecedor 100';
+        $fornecedor->site = 'fornecedor100.com.br';
+        $fornecedor->uf = 'CE';
+        $fornecedor->email = 'fornecedor100@email.com';
+        $fornecedor->save();
+
+        Fornecedor::create([
+            'nome' => 'Fornecedor 200',
+            'site' => 'fornecedor200.com.br',
+            'uf' => 'RS',
+            'email' => 'fornecedor200@email.com',
+        ]);
+    }
+}
