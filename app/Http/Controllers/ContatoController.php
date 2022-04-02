6af96e56aef447c0599e70f131e4765c067b06cb
Diego Rodrigues <diegokod@gmail.com>
2022-04-02 20:01:21 -0300

    validations

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,11 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\SiteContato;
 
 class ContatoController extends Controller
 {
-    public function contato() {
+    public function contato(Request $request) {
 
-        return view('site.contato');
+        // $contato = new SiteContato();
+        // $contato->nome = $request->input('nome');
+        // $contato->telefone = $request->input('telefone');
+        // $contato->email = $request->input('email');
+        // $contato->motivo_contato = $request->input('motivo_contato');
+        // $contato->mensagem = $request->input('mensagem');
+
+
+        // $contato->save();
+
+        // $contato = new SiteContato();
+        // $contato->create($request->all());
+        // $contato->save();
+
+        return view('site.contato', ['titulo' => 'Contato (teste)']);
+    }
+
+    public function salvar(Request $request) {
+
+        $request->validate([
+            'nome' => 'required',
+        ]);
+       SiteContato::create($request->all());
+
     }
 }
