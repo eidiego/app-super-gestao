1df38b8b78e4292ea1f94dce4d6a12cb4b0262a7
Diego Rodrigues <diegokod@gmail.com>
2022-04-03 12:45:24 -0300

    Form fornecedores ready

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Fornecedor;
 
 class FornecedorController extends Controller
 {
@@ -14,8 +15,33 @@ class FornecedorController extends Controller
         return view('app.fornecedor.listar');
     }
 
+    public function adicionar(Request $request) {
 
-    public function adicionar() {
+        if($request->input('_token') != '') {
+            $regras = [
+                'nome' => 'required|min:3|max:40',
+                'site' => 'required',
+                'uf' => 'required|min:2|max:2',
+                'email' => 'email',
+            ];
+
+            $feedback = [
+                'required' => 'O campo :attribute deve ser preenchido',
+                'nome.min' => 'O nome deve ter no minimo 3 caracteres',
+                'nome.max' => 'O nome deve ter no maximo 40 caracteres',
+                'uf.min' => 'O campo uf deve ter no minimo 2 caracteres',
+                'nomemin' => 'O campo uf deve ter no maximo 2 caracteres',
+                'email.email' => 'O campo e-mail está invalido'
+            ];
+
+            $request->validate($regras, $feedback);
+
+            $fornecedor = new Fornecedor();
+            $fornecedor->create($request->all());
+        }
+
+
+
         return view('app.fornecedor.adicionar');
     }
 
--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,7 @@ Route::middleware('autenticacao:padrao,visitante,p3,p4')->prefix('/app')->group(
     Route::get('/fornecedor', 'FornecedorController@index')->name('app.fornecedor');
     Route::post('/fornecedor/listar', 'FornecedorController@listar')->name('app.fornecedor.listar');
     Route::get('/fornecedor/adicionar', 'FornecedorController@adicionar')->name('app.fornecedor.adicionar');
+    Route::post('/fornecedor/adicionar', 'FornecedorController@adicionar')->name('app.fornecedor.adicionar');
 
 
     Route::get('/produto', 'ProdutoController@index')->name('app.produto');
