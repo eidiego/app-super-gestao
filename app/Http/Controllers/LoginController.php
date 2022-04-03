50ca27ac88e02bd5a5a20f33207e596ac315e971
Diego Rodrigues <diegokod@gmail.com>
2022-04-02 23:17:52 -0300

    Add login autentication

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,14 +3,47 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class LoginController extends Controller
 {
-    public function index() {
-        return view('site.login', ['titulo' => 'Login']);
+    public function index(Request $request) {
+
+
+        $erro = '';
+
+        if($request->get('erro') == 1){
+            $erro = 'Usuário ou senha não existe';
+        };
+        return view('site.login', ['titulo' => 'Login', 'erro' => $erro]);
+
     }
 
-    public function autenticar() {
-        return 'Chegamos até o autenticar!';
+    public function autenticar(Request $request) {
+
+        $regras = [
+            'usuario' => 'email',
+            'senha' => 'required',
+        ];
+
+        $feedback = [
+            'usuario.email' => 'O campo usuário (e-mail) é obrigatorio',
+            'senha.required' => 'O campo senha é obrigatorio',
+        ];
+
+        $request->validate($regras, $feedback);
+
+        $email = $request->get('usuario');
+        $password =  $request->get('senha');
+
+        $user = new User();
+        $usuario = $user->where('email', $email)->where('password', $password)->get()->first();
+
+        if(isset($usuario->name)) {
+            echo 'Usuario existe';
+        } else {
+            return redirect()->route('site.login', ['erro' => 1]);
+        }
+
     }
 }
