069853e65872150bd36d4bf27010983317e3ab0e
Diego Rodrigues <diegokod@gmail.com>
2022-04-02 21:54:54 -0300

    kernel middleware updated

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Middleware\LogAcessoMiddleware;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -19,9 +19,7 @@ Route::get('/', function () {
 });
 */
 
-Route::middleware(LogAcessoMiddleware::class)
-->get('/', 'PrincipalController@principal')
-->name('site.index');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
 Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 Route::post('/contato', 'ContatoController@salvar')->name('site.contato');
