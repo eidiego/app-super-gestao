1daf4fa51add7bad2eec244a65cab86d3558b351
Diego Rodrigues <diegokod@gmail.com>
2022-04-03 12:23:46 -0300

    Add fornecedor pages

---
+++ resources/views/app/fornecedor/index.blade.php
@@ -0,0 +1,33 @@
+@extends('app.layouts.basico')
+
+@section('titulo', 'Fornecedor')
+
+@section('conteudo')
+
+    <div class="conteudo-pagina">
+        <div class="titulo-pagina2 ">
+            <p>Fornecedor</p>
+        </div>
+
+        <div class="menu">
+            <ul>
+                <li><a href="#"> Novo </a></li>
+                <li><a href="#"> Consulta </a></li>
+            </ul>
+
+        </div>
+
+        <div class="informacao-pagina">
+            <div style="width:30%; margin-left: auto; margin-right: auto" >
+                <form method="post" action="">
+                    <input type="text" name="nome" placeholder="Nome" class="borda-preta">
+                    <input type="text" name="site" placeholder="Site" class="borda-preta">
+                    <input type="text" name="uf" placeholder="UF" class="borda-preta">
+                    <input type="text" name="email" placeholder="E-mail" class="borda-preta">
+                    <button types="submit" class="borda-preta"> Pesquisar </button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+@endsection
